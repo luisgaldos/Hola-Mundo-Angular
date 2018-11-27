9f51220e9f542ab00ef949e16881deb42048b80f
Curso <Curso@IPARTEKAULA.COM>
2018-11-27 13:58:03 +0100

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './componentes/home/home.component';
+import { FrutaComponent } from './componentes/fruta/fruta.component';
+import { Page404Component } from './componentes/page404/page404.component';
+import { AlumnosComponent } from './componentes/alumnos/alumnos.component';
+
+
+const routes: Routes = [
+  { path: 'home', component : HomeComponent },
+  { path: 'fruta', component : FrutaComponent },
+  { path: 'alumnos', component : AlumnosComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component : Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { 
+  MatToolbarModule,  
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule
+ } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { AlumnosComponent } from './alumnos/alumnos.component';
 import { FlujoInformacionComponent } from './flujo-informacion/flujo-informacion.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { Page404Component } from './componentes/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrutaComponent,
     AlumnosComponent,
-    FlujoInformacionComponent
+    FlujoInformacionComponent,
+    HomeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
